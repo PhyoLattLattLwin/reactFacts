27bf4da8b7e0a1e4ed95187b868036f498ea4273
PhyoLattLattLwin <phyolatlatlwin@gicjp.com>
2023-08-16 09:22:19 +0630

    reactFacts

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      textblue:'#00B4D5',
+      black:'#21222A',
+      white:'#fff',
+      black2:'#282D35'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
